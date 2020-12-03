5cddbf738244afa8460793eae10d1323ecf3b5f3
mfontela <mfontela@iim.csic.es>
2020-12-03 16:43:45 +0000

    staging all files

---
+++ build_site.R
@@ -0,0 +1,16 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("C:/Users/MFontela/Documents/web")
+
+#render your sweet site. 
+rmarkdown::render_site(encoding = 'UTF-8')
+
+# #NOTA: después de generarlo, lo mejor es copiar todo el contenido
+# de la PUTA carpeta _site en el genérico site2020 (un nivel anterior), tambien la carpeta site_libs
+# y hacer un shell (entrando desde la rueda dentada de arriba a la derecha) pegando: 
+# git add -A && git commit -m 'staging all files' 
+# y luego haciendo un push (flecha verde)
+
+#Otro link importante: https://login.ionos.es/ (lo del dominio)
