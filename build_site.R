9fbaa11ceedf62e5ba9eee0ddac27d60080d2461
mfontela <mfontela@iim.csic.es>
2020-12-03 17:11:52 +0000

    staging all files

--- build_site.R
+++ build_site.R
@@ -8,7 +8,7 @@ setwd("C:/Users/MFontela/Documents/web")
 rmarkdown::render_site(encoding = 'UTF-8')
 
 # #NOTA: después de generarlo, lo mejor es copiar todo el contenido
-# de la PUTA carpeta _site en el genérico site2020 (un nivel anterior), tambien la carpeta site_libs
+# de la PUTA carpeta _site en el genérico web (un nivel anterior), tambien la carpeta site_libs
 # y hacer un shell (entrando desde la rueda dentada de arriba a la derecha) pegando: 
 # git add -A && git commit -m 'staging all files' 
 # y luego haciendo un push (flecha verde)
