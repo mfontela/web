f105fb76428a186dda6ea3e5187123bb6e18d228
mfontela <marcosmfontela@gmail.com>
2022-10-16 21:11:46 +0200

    staging all files

--- build_site.R
+++ build_site.R
@@ -1,7 +1,4 @@
 #Set our working directory. 
-#This helps avoid confusion if our working directory is 
-#not our site because of other projects we were 
-#working on at the time. 
 setwd("C:/Users/MFontela/Documents/web")
 
 #render your site. 
