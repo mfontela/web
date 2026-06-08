2793e3fbb71e177735f7d52bd5395dbd2d9a01ae
Marcos Fontela <marcosmfontela@gmail.com>
2026-06-08 07:57:02 +0200

    .

---
+++ build_site (SFConflict Marcos Fontela 2026-05-07-10-55-50).R
@@ -0,0 +1,17 @@
+#Set our working directory. 
+setwd("C:/Users/marco/Seafile/marcosmfontela/web")
+
+#render your site. 
+rmarkdown::render_site(encoding = 'UTF-8')
+
+# #NOTA: después de generarlo, lo mejor es copiar todo el contenido
+# de la PUTA carpeta _site en el genérico web (un nivel anterior), tambien la carpeta site_libs
+# y hacer un shell New terminal... (entrando desde la rueda dentada de arriba a la derecha "rueda More") pegando: 
+# git add -A && git commit -m 'staging all files'
+# y luego haciendo un push (flecha verde)
+#UPDATE May 2026: es probable que haya resuelto este paso de manera más "limpia".
+
+#Otro link importante: https://login.ionos.es/ (lo del dominio)
+
+#Otro tip: si las fotos pierden la posición centrada, repasa las <p align="center"> </p> del html (que nos las duplique)
+#TIP RyC y similares: link a los pdfs que se llamen igual y sustituyes uno por otro
